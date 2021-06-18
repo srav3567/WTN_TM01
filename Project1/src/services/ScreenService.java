d379663b86622cfecb909370a821bebbfc34f717
sravanthi <sravanthi@DESKTOP-2LCUV1T>
2021-06-18 10:59:57 +0530

    Virtual  Key Application

---
+++ Project1/src/services/ScreenService.java
@@ -0,0 +1,34 @@
+package services;
+
+import screens.FileOptionsScreen;
+import screens.Screen;
+import screens.WelcomeScreen;
+
+
+
+public class ScreenService {
+	
+	//static Directory dir = new Directory();
+	
+	
+	
+	public static WelcomeScreen WelcomeScreen = new WelcomeScreen();
+    public static FileOptionsScreen FileOptionsScreen = new FileOptionsScreen();
+    
+    
+
+    public static Screen CurrentScreen = WelcomeScreen;
+
+    
+    public static Screen getCurrentScreen() {
+        return CurrentScreen;
+    }
+
+    
+    public static void setCurrentScreen(Screen currentScreen) {
+        CurrentScreen = currentScreen;
+    }
+    
+    
+    
+}
